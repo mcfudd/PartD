44177c8aa6c617169b2736b01779550220c685f5
mcfudd <christian.mans@gmail.com>
2020-09-08 20:27:48 +1200

    added code to print best day of week

--- PartD/src/PartD.java
+++ PartD/src/PartD.java
@@ -6,5 +6,6 @@ public class PartD
 		System.out.println("Hi, my name is Christian Mans");
 		System.out.println("My favourite colour is blue");
 		System.out.println("My favourite mode of transportation is motorcycle");
+		System.out.println("The best day of the week is Tuesday");
 	}
 }
